bff274fdd849192eea7de7e15ea0ce878709b09d
4nar1k <anarik.pro@gmail.com>
2025-01-30 12:32:07 +0400

    Added POST request

--- main.go
+++ main.go
@@ -1,17 +1,35 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
 	"net/http"
 )
 
-func HelloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello, World!")
+var task string
+
+type TaskRequest struct {
+	Task string `json:"task"`
 }
 
+func GetHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %s", task)
+}
+func PostHandler(w http.ResponseWriter, r *http.Request) {
+	var taskReq TaskRequest
+	err := json.NewDecoder(r.Body).Decode(&taskReq)
+	if err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+	task = taskReq.Task
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, "Task updated successfully")
+}
 func main() {
 	router := mux.NewRouter()
-	router.HandleFunc("/api/hello", HelloHandler).Methods("GET")
+	router.HandleFunc("/api/hello", GetHandler).Methods("GET")
+	router.HandleFunc("/api/hello", PostHandler).Methods("POST")
 	http.ListenAndServe(":8080", router)
 }
