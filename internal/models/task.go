3075d0a0426ccd7109fadef0e62c58c9b10da770
4nar1k <anarik.pro@gmail.com>
2025-03-06 20:47:42 +0400

    Добавил связь с каскадным удалением + UserID для связи задачи с её владельцем.

--- internal/models/task.go
+++ internal/models/task.go
@@ -7,4 +7,6 @@ type Task struct {
 	gorm.Model
 	Task   string `json:"task"`
 	IsDone bool   `json:"is_done"`
+	UserID uint   `json:"user_id" gorm:"not null;index"`
+	User   User   `gorm:"constraint:OnDelete:CASCADE;"` // Связь с каскадным удалением
 }
