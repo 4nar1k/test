f731d134242c738a19cb601d6289ae0902842a95
4nar1k <anarik.pro@gmail.com>
2025-03-06 20:36:10 +0400

    Добавил возвращение списка задач конкретного пользователя GetUsersIdTasks

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -5,14 +5,13 @@ import (
 	"apiRUKA/internal/taskService"
 	"apiRUKA/internal/web/tasks"
 	"context"
+	"errors"
 )
 
-// Handler структура, которая реализует StrictServerInterface
 type Handler struct {
 	Service *taskService.TaskService
 }
 
-// NewHandler создает новый экземпляр Handler
 func NewTaskHandler(service *taskService.TaskService) *Handler {
 	return &Handler{
 		Service: service,
@@ -32,75 +31,93 @@ func (h *Handler) getTasks(ctx context.Context, request tasks.GetTasksRequestObj
 
 	response := tasks.GetTasks200JSONResponse{}
 	for _, tsk := range allTasks {
+		userId := int(tsk.UserID) // Преобразование uint в int
 		task := tasks.Task{
 			Id:     &tsk.ID,
 			Task:   &tsk.Task,
 			IsDone: &tsk.IsDone,
+			UserId: &userId,
 		}
 		response = append(response, task)
 	}
 	return response, nil
 }
 
-// PostTasks создает новую задачу
-func (h *Handler) PostTasks(ctx context.Context, request tasks.PostTasksRequestObject) (tasks.PostTasksResponseObject, error) {
-	return h.postTasks(ctx, request)
+// GetUsersIdTasks возвращает список задач конкретного пользователя
+func (h *Handler) GetUsersIdTasks(ctx context.Context, request tasks.GetUsersIdTasksRequestObject) (tasks.GetUsersIdTasksResponseObject, error) {
+	tasksByUser, err := h.Service.GetTasksByUserID(uint(request.Id))
+	if err != nil {
+		return nil, err
+	}
+
+	response := tasks.GetUsersIdTasks200JSONResponse{}
+	for _, tsk := range tasksByUser {
+		userId := int(tsk.UserID)
+		task := tasks.Task{
+			Id:     &tsk.ID,
+			Task:   &tsk.Task,
+			IsDone: &tsk.IsDone,
+			UserId: &userId,
+		}
+		response = append(response, task)
+	}
+	return response, nil
 }
 
-func (h *Handler) postTasks(ctx context.Context, request tasks.PostTasksRequestObject) (tasks.PostTasksResponseObject, error) {
-	taskRequest := request.Body
-	taskToCreate := models.Task{
-		Task:   *taskRequest.Task,
-		IsDone: *taskRequest.IsDone,
+func (h *Handler) PostTasks(ctx context.Context, request tasks.PostTasksRequestObject) (tasks.PostTasksResponseObject, error) {
+	if request.Body.UserId == nil {
+		return nil, errors.New("user_id is required")
 	}
+
+	taskToCreate := models.Task{
+		Task:   *request.Body.Task,
+		IsDone: *request.Body.IsDone,
+		UserID: uint(*request.Body.UserId), // Преобразование int в uint
+	}
+
 	createdTask, err := h.Service.CreateTask(taskToCreate)
 	if err != nil {
 		return nil, err
 	}
 
+	userId := int(createdTask.UserID)
 	response := tasks.PostTasks201JSONResponse{
 		Id:     &createdTask.ID,
 		Task:   &createdTask.Task,
 		IsDone: &createdTask.IsDone,
+		UserId: &userId,
 	}
+
 	return response, nil
 }
 
 // PatchTasksId обновляет задачу по ID
 func (h *Handler) PatchTasksId(ctx context.Context, request tasks.PatchTasksIdRequestObject) (tasks.PatchTasksIdResponseObject, error) {
-	return h.patchTasksId(ctx, request)
-}
-
-func (h *Handler) patchTasksId(ctx context.Context, request tasks.PatchTasksIdRequestObject) (tasks.PatchTasksIdResponseObject, error) {
-	taskID := request.Id
-	taskRequest := request.Body
-
-	taskToUpdate := models.Task{
-		Task:   *taskRequest.Task,
-		IsDone: *taskRequest.IsDone,
+	if request.Body == nil {
+		return nil, errors.New("request body is empty")
 	}
-
-	updatedTask, err := h.Service.UpdateTaskByID(uint(taskID), taskToUpdate)
+	taskToUpdate := models.Task{
+		Task:   *request.Body.Task,
+		IsDone: *request.Body.IsDone,
+	}
+	updatedTask, err := h.Service.UpdateTaskByID(uint(request.Id), taskToUpdate)
 	if err != nil {
 		return nil, err
 	}
 
+	userId := int(updatedTask.UserID)
 	response := tasks.PatchTasksId200JSONResponse{
 		Id:     &updatedTask.ID,
 		Task:   &updatedTask.Task,
 		IsDone: &updatedTask.IsDone,
+		UserId: &userId,
 	}
 	return response, nil
 }
 
 // DeleteTasksId удаляет задачу по ID
 func (h *Handler) DeleteTasksId(ctx context.Context, request tasks.DeleteTasksIdRequestObject) (tasks.DeleteTasksIdResponseObject, error) {
-	return h.deleteTasksId(ctx, request)
-}
-
-func (h *Handler) deleteTasksId(ctx context.Context, request tasks.DeleteTasksIdRequestObject) (tasks.DeleteTasksIdResponseObject, error) {
-	taskID := request.Id
-	err := h.Service.DeleteTaskByID(uint(taskID))
+	err := h.Service.DeleteTaskByID(uint(request.Id))
 	if err != nil {
 		return nil, err
 	}
