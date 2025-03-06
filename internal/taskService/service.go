0f22f9251d6c4a78a3716da8b2b32269d52632dc
4nar1k <anarik.pro@gmail.com>
2025-03-06 20:57:18 +0400

    Добавил проверку user_id в CreateTask и метод GetTasksByUserID

--- internal/taskService/service.go
+++ internal/taskService/service.go
@@ -1,6 +1,9 @@
 package taskService
 
-import "apiRUKA/internal/models"
+import (
+	"apiRUKA/internal/models"
+	"fmt"
+)
 
 type TaskService struct {
 	repo TaskRepository
@@ -11,11 +14,18 @@ func NewTaskService(repo TaskRepository) *TaskService {
 }
 
 func (s *TaskService) CreateTask(task models.Task) (models.Task, error) {
+	if task.UserID == 0 {
+		return models.Task{}, fmt.Errorf("user_id is required")
+	}
 	return s.repo.CreateTask(task)
 }
 
 func (s *TaskService) GetAllTasks() ([]models.Task, error) {
 	return s.repo.GetAllTasks()
+}
+
+func (s *TaskService) GetTasksByUserID(userID uint) ([]models.Task, error) {
+	return s.repo.GetTasksByUserID(userID)
 }
 
 func (s *TaskService) UpdateTaskByID(id uint, task models.Task) (models.Task, error) {
