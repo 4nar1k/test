e283bd9139b3c3fae239e5be990bc0b165645f02
4nar1k <anarik.pro@gmail.com>
2025-03-06 20:24:49 +0400

    Добавил метод GetTasksForUser

--- internal/userService/service.go
+++ internal/userService/service.go
@@ -1,13 +1,17 @@
 package userService
 
-import "apiRUKA/internal/models"
+import (
+	"apiRUKA/internal/models"
+	"apiRUKA/internal/taskService"
+)
 
 type UserService struct {
-	repo UserRepository
+	repo        UserRepository
+	taskService *taskService.TaskService
 }
 
-func NewUserService(repo UserRepository) *UserService {
-	return &UserService{repo: repo}
+func NewUserService(repo UserRepository, taskService *taskService.TaskService) *UserService {
+	return &UserService{repo: repo, taskService: taskService}
 }
 
 func (s *UserService) CreateUser(user models.User) (models.User, error) {
@@ -27,5 +31,10 @@ func (s *UserService) UpdateUserByID(id uint, user models.User) (models.User, er
 }
 
 func (s *UserService) DeleteUserByID(id uint) error {
-	return s.repo.DeleteUserByID(id)
+	db := s.repo.GetDB()
+	return db.Unscoped().Delete(&models.User{}, id).Error
+}
+
+func (s *UserService) GetTasksForUser(userID uint) ([]models.Task, error) {
+	return s.taskService.GetTasksByUserID(userID)
 }
