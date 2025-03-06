c793e4e58111055edad7521e47ffa942a1d5eb57
4nar1k <anarik.pro@gmail.com>
2025-03-06 20:55:11 +0400

    Добавил GetTasksByUserID

--- internal/taskService/repository.go
+++ internal/taskService/repository.go
@@ -6,17 +6,13 @@ import (
 )
 
 type TaskRepository interface {
-	// CreateTask - Передаем в функцию task типа Task из orm.go
-	// возвращаем созданный Task и ошибку
 	CreateTask(task models.Task) (models.Task, error)
-	// GetAllTasks - Возвращаем массив из всех задач в БД и ошибку
 	GetAllTasks() ([]models.Task, error)
-	// UpdateTaskByID - Передаем id и Task, возвращаем обновленный Task
-	// и ошибку
+	GetTasksByUserID(userID uint) ([]models.Task, error) //
 	UpdateTaskByID(id uint, task models.Task) (models.Task, error)
-	// DeleteTaskByID - Передаем id для удаления, возвращаем только ошибку
 	DeleteTaskByID(id uint) error
 }
+
 type taskRepository struct {
 	db *gorm.DB
 }
@@ -25,7 +21,6 @@ func NewTaskRepository(db *gorm.DB) TaskRepository {
 	return &taskRepository{db: db}
 }
 
-// (r *taskRepository) привязывает данную функцию к нашему репозиторию
 func (r *taskRepository) CreateTask(task models.Task) (models.Task, error) {
 	result := r.db.Create(&task)
 	if result.Error != nil {
@@ -33,24 +28,28 @@ func (r *taskRepository) CreateTask(task models.Task) (models.Task, error) {
 	}
 	return task, nil
 }
+
 func (r *taskRepository) GetAllTasks() ([]models.Task, error) {
 	var tasks []models.Task
 	err := r.db.Find(&tasks).Error
 	return tasks, err
 }
+
+func (r *taskRepository) GetTasksByUserID(userID uint) ([]models.Task, error) {
+	var tasks []models.Task
+	err := r.db.Where("user_id = ?", userID).Find(&tasks).Error
+	return tasks, err
+}
+
 func (r *taskRepository) UpdateTaskByID(id uint, task models.Task) (models.Task, error) {
 	var existing models.Task
-	//  Проверяем, существует ли задача в базе
 	if err := r.db.First(&existing, id).Error; err != nil {
-		return models.Task{}, err // Если нет, возвращаем ошибку
+		return models.Task{}, err
 	}
-
-	//   Обновляем только переданные поля
 	result := r.db.Model(&existing).Updates(task)
 	if result.Error != nil {
 		return models.Task{}, result.Error
 	}
-
 	return existing, nil
 }
 
